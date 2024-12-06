a7f25d8fd6dbf59f773e3e44d1aff72cb32552c3
KodekaTeam <kodekateam@gmail.com>
2024-12-06 19:14:02 +0700

    update: version app

--- include/version.php
+++ include/version.php
@@ -16,6 +16,6 @@ if (!isset($_SESSION["mikhmon"])) {
         // $_SESSION["vm-old"] = "1.00 09-01-2023";
         // $_SESSION["vm"] = "1.06 12-09-2024";
         $_SESSION["vm-old"] = "1.02 09-09-2024";
-        $_SESSION["vm"] = "1.06.06122024 12-06-2024";
+        $_SESSION["vm"] = "1.06.08122024 12-06-2024";
     /* Version ChangeLog */
     }
