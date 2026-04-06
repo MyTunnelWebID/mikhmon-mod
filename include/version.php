ea41fdb1d26a44c0fc0bae886ff548424ad46326
KodekaTeam <kodekateam@gmail.com>
2026-04-06 09:48:15 +0700

    update versioning: change version to v2.00 Batch-06042026 and update release date

--- include/version.php
+++ include/version.php
@@ -3,16 +3,16 @@ if (!isset($_SESSION["mikhmon"])) {
     header("Location:../admin.php?id=login");
   } else {
     /* Version ChangeLog */
-        $_SESSION["copyright-mod"] = "2023 MyTunnel Teams";
-        $_SESSION["author-mod"] = "MyTunnel Teams";
-        $_SESSION["licence-mod"] = "No Licence";
+        $_SESSION["copyright-mod"] = "2023 - " . date("Y") ." MyTunnel Team";
+        $_SESSION["author-mod"] = "MyTunnel Team";
+        $_SESSION["licence-mod"] = "MIT License";
         $_SESSION["website-mod"] = "https://mytunnel.web.id";
         $_SESSION["facebook-mod"] = "https://www.facebook.com/mytunnelwebid";
     /* Version ChangeLog */
 
 
     /* Version ChangeLog */
-        // $_SESSION["v"] = "3.20 06-30-2021";
+        $_SESSION["v"] = "3.20 06-30-2021";
         
         // $_SESSION["vm-old"] = "1.00 09-01-2023";
         // $_SESSION["vm"] = "1.06 12-09-2024";
@@ -20,7 +20,10 @@ if (!isset($_SESSION["mikhmon"])) {
         // $_SESSION["vm-old"] = "1.02 09-09-2024";
         // $_SESSION["vm"] = "1.06.06122024 12-06-2024";
 
-        $_SESSION["vm-old"] = "1.02 09-09-2024";
-        $_SESSION["vm"] = "1.10.20052025 20-05-2025";
+        // $_SESSION["vm-old"] = "1.02 09-09-2024";
+        // $_SESSION["vm"] = "1.10.20052025 20-05-2025";
+
+        $_SESSION["vm-old"] = "1.10 20-05-2025";
+        $_SESSION["vm"] = "2.00 06-04-2026";
     /* Version ChangeLog */
     }
